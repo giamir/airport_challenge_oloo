2413a6a5acceb25d191efc92fc4b7610ca2b90e8
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2016-04-26 18:06:48 +0100

    implement land method for Airport object

--- src/airport.js
+++ src/airport.js
@@ -4,15 +4,41 @@ module.exports = (function () {
   var uid = 0;
   var DEFAULT_CAPACITY = 20;
 
-  Airport.init = function(capacity) {
-    capacity = typeof capacity !== 'undefined' ? capacity : DEFAULT_CAPACITY;
+  Airport.init = function(opts) {
+    opts.capacity = typeof opts.capacity !== 'undefined' ? opts.capacity : DEFAULT_CAPACITY;
+    opts.weatherObj = typeof opts.weatherObj !== 'undefined' ? opts.weatherObj : require('./weather');
     privateStore[this.id = uid++] = {};
-    privateStore[this.id].capacity = capacity;
+    privateStore[this.id].capacity = opts.capacity;
+    privateStore[this.id].weather = Object.create(opts.weatherObj);
+    privateStore[this.id].planes = [];
   };
 
   Airport.capacity = function() {
     return privateStore[this.id].capacity;
   };
+
+  Airport.planes = function() {
+    return privateStore[this.id].planes;
+  };
+
+  Airport.land = function(plane) {
+    if (isStormy.call(this)) { throw new Error('Unable to land due to stormy weather'); }
+    if (isFull.call(this)) { throw new Error('Unable to land cause airport is full'); }
+    plane.land();
+    addPlane.call(this, plane);
+  };
+
+  function addPlane(plane) {
+    privateStore[this.id].planes.push(plane);
+  }
+
+  function isFull() {
+    return privateStore[this.id].planes.length >= privateStore[this.id].capacity;
+  }
+
+  function isStormy() {
+    return privateStore[this.id].weather.isStormy();
+  }
 
   return Airport;
 })();
--- test/unit/airport.spec.js
+++ test/unit/airport.spec.js
@@ -2,10 +2,14 @@ var Airport = require('../../src/airport');
 
 describe('Airport', function() {
   var airport;
+  var planeMock;
+  var weatherObjMock;
 
   beforeEach(function() {
-    airport = Object.create(Airport)
-    airport.init();
+    planeMock = jasmine.createSpyObj('plane', ['land', 'takeOff']);
+    weatherObjMock = jasmine.createSpyObj('weatherObj', ['isStormy']);
+    airport = Object.create(Airport);
+    airport.init({ weatherObj: weatherObjMock });
   });
 
   describe('#init', function() {
@@ -13,9 +17,32 @@ describe('Airport', function() {
       expect(airport.capacity()).toEqual(20); // retrieve constant from airport closure is not possible
     });
     it('initialize airport with a specified capacity', function() {
-      var airportAux = Object.create(Airport)
-      airportAux.init(10);
+      var airportAux = Object.create(Airport);
+      airportAux.init({ capacity: 10 });
       expect(airportAux.capacity()).toEqual(10);
     });
   });
+
+  describe('#land', function() {
+   it('instructs the plane to land', function() {
+     airport.land(planeMock);
+     expect(planeMock.land).toHaveBeenCalled();
+   });
+   it('has the plane after it has landed', function() {
+     airport.land(planeMock);
+     expect(airport.planes()).toContain(planeMock);
+   });
+   it('does not allow the plane to land when weather is stormy', function() {
+     weatherObjMock.isStormy.and.returnValue(true);
+     var msg = 'Unable to land due to stormy weather';
+     expect( function(){ airport.land(planeMock); } ).toThrowError(msg);
+   });
+   it('does not allow the plane to land when airport is full', function() {
+     for (var i = 0; i < airport.capacity(); i++) {
+       airport.land(planeMock);
+     }
+     var msg = 'Unable to land cause airport is full';
+     expect( function(){ airport.land(planeMock); } ).toThrowError(msg);
+   });
+ });
 });
